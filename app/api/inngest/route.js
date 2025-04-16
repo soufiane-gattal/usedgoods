037b89165dfe7c936c6be01ebb2094dcbd773aa7
soufiane <soufiangattal3.com>
2025-04-16 21:29:57 +0200

    First Commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,5 @@
 import { serve } from "inngest/next";
 import { inngest, syncUserCreation,syncUserUpdation ,syncUserDeletion } from "@/config/inngest";
-
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
